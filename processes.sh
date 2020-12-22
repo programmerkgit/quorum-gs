cfde0c8932ba7059009cc66288c123002dc77f7d
admin <keishi.kubo@digglue.com>
2020-12-22 17:19:34 +0900

    updated implementation

--- processes.sh
+++ processes.sh
@@ -3,13 +3,14 @@
 readonly filename=".db/processes"
 function removeNotExistProcessesFromFile() {
   # 各行についてプロセスの存在の確認を行う
+  psax="$(ps ax -o command,pid)"
   processes=$(while read line; do
     # PIDを設定
     set $line
     pid=$1
 
     # プロセスが存在する場合のみラインを記述
-    ps ax -o command,pid | grep geth | grep "$pid" > /dev/null 2>&1
+    echo "$psax" | grep geth | grep "$pid" >/dev/null 2>&1
     if [ $? -eq 0 ]; then
       echo "$line"
     fi
